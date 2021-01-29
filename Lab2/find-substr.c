fd3eaeaba1260158181e61f066045be235e3d11c
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2021-01-29 14:17:38 -0500

    :(

--- Lab2/find-substr.c
+++ Lab2/find-substr.c
@@ -4,25 +4,34 @@
 
 
 #include <stdio.h>
+#include <string.h>
+
 
 void find_occurance(char* haystack, char* needle) {
+//	printf("Needle: %s\n", needle);
+//	printf("Haystack: %s\n", haystack);
 	
-	// logic
-}
-
-
+	char* position = strstr(haystack, needle);
+	
+	if (position) {
+		printf("yes\n");		
+	} else {
+		printf("Occurance not found!\n");
+	}
+}	
 
 
 
 int main() {
 
 	printf("Time to play Needle in a Haystack!\n");
-	printf("Enter a needle:\n");
 
-	char haystack[] = "Hello, welcome to CS5007!\n";
-//	printf("%
-
-
+	char haystack[] = "Hello, welcome to CS5007!";
+	char needle1[] = "el";
+	char needle2[] = "Jovi";
+	
+//	find_occurance(haystack, needle1);
+	find_occurance(haystack, needle2);
 
 	return 0;
 }
