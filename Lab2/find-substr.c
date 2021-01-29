255bdc725a64820e0b3e9ccc1b3e6f2e0af859f8
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2021-01-29 10:50:57 -0500

    saving lab code

---
+++ Lab2/find-substr.c
@@ -0,0 +1,32 @@
+// Joviane Bellegarde
+//
+//
+
+
+#include <stdio.h>
+
+void find_occurance(char* haystack, char* needle) {
+	
+	// logic
+}
+
+
+
+
+
+int main() {
+
+	printf("Time to play Needle in a Haystack!\n");
+	printf("Enter a needle: ");
+
+	char haystack[25] = "Hello, welcome to CS5007!";
+
+
+
+
+	return 0;
+}
+
+
+
+
