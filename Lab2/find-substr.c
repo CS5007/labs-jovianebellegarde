21b7f737edb47cd58fbb042ec011a39289f0a917
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2021-01-29 11:06:12 -0500

    fixing code

--- Lab2/find-substr.c
+++ Lab2/find-substr.c
@@ -17,10 +17,10 @@ void find_occurance(char* haystack, char* needle) {
 int main() {
 
 	printf("Time to play Needle in a Haystack!\n");
-	printf("Enter a needle: ");
+	printf("Enter a needle:\n");
 
-	char haystack[25] = "Hello, welcome to CS5007!";
-
+	char haystack[] = "Hello, welcome to CS5007!\n";
+//	printf("%
 
 
 
