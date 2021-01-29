ae15e329d83ba752cb3913638b64007c45efdaab
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2021-01-29 15:05:41 -0500

    Partial lab done

--- Lab2/find-substr.c
+++ Lab2/find-substr.c
@@ -8,18 +8,17 @@
 
 
 void find_occurance(char* haystack, char* needle) {
-//	printf("Needle: %s\n", needle);
-//	printf("Haystack: %s\n", haystack);
-	
 	char* position = strstr(haystack, needle);
 	
-	if (position) {
-		printf("yes\n");		
-	} else {
-		printf("Occurance not found!\n");
-	}
-}	
+	// the only works for first position :/
 
+	if (position) {
+		printf("Occurrence found at position: %ld\n", position - haystack);	
+	} else {
+		printf("Occurrence not found!\n");
+	}
+	
+}	
 
 
 int main() {
@@ -30,7 +29,7 @@ int main() {
 	char needle1[] = "el";
 	char needle2[] = "Jovi";
 	
-//	find_occurance(haystack, needle1);
+	find_occurance(haystack, needle1);
 	find_occurance(haystack, needle2);
 
 	return 0;
