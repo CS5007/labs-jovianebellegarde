cbb9fc5769eb2e3a7c8263dddc81eeadadc9336d
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2021-01-31 22:43:28 -0500

    Got it to kinda work

--- Lab2/find-substr.c
+++ Lab2/find-substr.c
@@ -1,7 +1,5 @@
 // Joviane Bellegarde
-// Used https://cboard.cprogramming.com/c-programming/73365-how-use-strstr-find-all-occurrences-substring-string-not-only-first.html
-// as reference!
-//
+// Used https://www.tutorialspoint.com/c_standard_library/c_function_strcmp.htm for reference
 
 
 #include <stdio.h>
@@ -9,30 +7,39 @@
 
 
 void find_occurance(char* haystack, char* needle) {
-	//char* position = strstr(haystack, needle);
-	
-	// the only works for first position 
-
 	int count = 0;
-	
-	while ((haystack = strstr(haystack, needle)) != NULL) {
-		haystack += strlen(needle);
-		count++;
-		}	
-	printf("Found %d occurrences of the substring:'%s'\n", count, needle);
-}
+
+	// looping through the length of the haystack string
+	for (int i = 0; i<= strlen(haystack); i++) {
+
+		// looping through the first matched character in substring
+		if (haystack[i] == needle[0]) {
+
+			// looping through the second matched character in substring
+			if (haystack[i + 1] == needle[1]) {
+				++count;
+				printf("Occurrence found at position %d\n", i);
+		
+			} else {
+				printf("Occurrence not found!\n");
+				break;
+			}
+		}
+	}
+	printf("Found %d occurrences of the substring: '%s' in %s\n", count, needle, haystack);
+}	
 
 
 int main() {
 
-	printf("Time to play Needle in a Haystack!\n");
+	printf("Time to play Needle in a Haystack!\n\n");
 
 	char haystack[] = "Hello, welcome to CS5007!";
 	char needle1[] = "el";
 	char needle2[] = "Jovi";
 	
 	find_occurance(haystack, needle1);
-	//find_occurance(haystack, needle2);
+	find_occurance(haystack, needle2);
 
 	return 0;
 }
