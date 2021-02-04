e31d91aa9fc1307e3c71e213c42eb5d6cd1f0d48
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2021-02-04 17:06:30 -0500

    got the logic down, but in main

--- Lab3/linked-chars.c
+++ Lab3/linked-chars.c
@@ -5,7 +5,19 @@
 
 int main() {
 
+	FILE* filePointer;
 
+	filePointer = fopen("alphabet.txt", "r");
+
+	char buffer; 
+
+	while(1 == fscanf(filePointer, "%c", &buffer)) {
+		if (buffer == 'b') {
+			printf("The character 'b' is the second letter in the alphabet.\n");
+		}
+
+	}
+	fclose(filePointer);
 
 	return 0;
 
