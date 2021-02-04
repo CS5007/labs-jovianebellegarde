c1936d00654251cea61dbef5c8c7563316b36aee
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2021-02-04 16:47:37 -0500

    adding lab doc

--- Lab3/linked-chars.c
+++ Lab3/linked-chars.c
@@ -1,22 +1,12 @@
-// Joviane Bellegarde
-//
-//
+// Joviane Bellegarde 2/4/21
 //
 //
 #include <stdio.h>
-typedef struct dll_node{
-
-	char data;
-	struct node* next
-	struct node* prev;
-}dll_node_t;
-
 
 int main() {
 
 
 
-
-
 	return 0;
+
 }
