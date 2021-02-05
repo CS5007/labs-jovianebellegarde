df125db3a7e6b95f76ba61cf7e9258a2b0dc7d13
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2021-02-05 08:18:11 -0500

    starting over lol

--- Lab3/linked-chars.c
+++ Lab3/linked-chars.c
@@ -1,24 +1,12 @@
-// Joviane Bellegarde 2/4/21
+// Joviane Bellegarde
 //
 //
-#include <stdio.h>
+//
+# include <stdio.h>
+
 
 int main() {
 
-	FILE* filePointer;
-
-	filePointer = fopen("alphabet.txt", "r");
-
-	char buffer; 
-
-	while(1 == fscanf(filePointer, "%c", &buffer)) {
-		if (buffer == 'b') {
-			printf("The character 'b' is the second letter in the alphabet.\n");
-		}
-
-	}
-	fclose(filePointer);
 
 	return 0;
-
 }
