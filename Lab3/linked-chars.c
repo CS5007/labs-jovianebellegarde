1e905f210a0f247aab8ca772a442c82909839ed1
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2021-02-05 10:22:51 -0500

    examples from head first

--- Lab3/linked-chars.c
+++ Lab3/linked-chars.c
@@ -7,6 +7,15 @@
 
 int main() {
 
+	int x = 4;
+	printf("x is stored at %p\n", &x);
+	
+	int *address_of_x = &x;
+	printf("the address of x is %p\n", address_of_x);
 
-	return 0;
+	int value_stored = *address_of_x;
+	printf("the value of x is %d\n", value_stored);
+
+	*address_of_x = 99;
+	printf("The value of x is %d\n", *address_of_x);
 }
