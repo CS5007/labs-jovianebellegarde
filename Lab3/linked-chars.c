ae3cee7b4bb548b78d8e487692996b442161655e
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2021-02-07 11:15:37 -0500

    made struct ll_node

--- Lab3/linked-chars.c
+++ Lab3/linked-chars.c
@@ -4,26 +4,27 @@
 //
 # include <stdio.h>
 
-
-typedef struct alphabet {
-	char* letter;
+typedef struct ll_node {
 	int position;
-}alphabet;
-
+	char* letter;
+	struct node* next;
+}ll_node_t;
 
 
 int main() {
 
 	FILE* filePointer;
 	filePointer = fopen("alphabet.txt", "r");
+	char buffer;// holds character values
 	
+	struct node* alpha;
+	
+
 	while(1 == fscanf(filePointer, "%c", &buffer)) {
-		
-	
+		struct node* next;
+
+		 
 	}
-	
-
-	
-
+	fclose(filePointer);
 	return 0;	
 }
