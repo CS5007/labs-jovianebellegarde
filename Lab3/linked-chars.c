f365b5405af7b5e9862373f63bbb2e7c5384586e
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2021-02-07 10:22:45 -0500

    saving text and c files

--- Lab3/linked-chars.c
+++ Lab3/linked-chars.c
@@ -23,25 +23,7 @@ int main() {
 	}
 	
 
-	alphabet character;
-	alphabet.letter = "a";
-	alphabet.position = 1;
 	
-	printf("Letter: %c\n", alphabet.letter);
-	printf("Position %d\n", position);
-	/*
-	int x = 4;
-	printf("x is stored at %p\n", &x);
-	
-	int *address_of_x = &x;
-	printf("the address of x is %p\n", address_of_x);
-
-	int value_stored = *address_of_x;
-	printf("the value of x is %d\n", value_stored);
-
-	*address_of_x = 99;
-	printf("The value of x is %d\n", *address_of_x);
-	*/
 
 	return 0;	
 }
