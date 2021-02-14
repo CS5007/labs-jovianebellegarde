d266791fdd63f472cafdb6aef651eb5dfdcd1b8c
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2021-02-14 13:27:04 -0500

    added code to lab

--- Lab4/min_of_two.c
+++ Lab4/min_of_two.c
@@ -1,1 +1,24 @@
 // Joviane Bellegarde
+// Used this resource for the parens. 
+// I knew about ternary operators from Java, but wasn't aware of paren placement.
+// https://www.geeksforgeeks.org/macros-and-its-types-in-c-cpp/
+//
+//
+#include <stdio.h>
+
+#define MINOF2(num1, num2) ((num1) < (num2) ? (num1) : (num2))
+
+int main() {
+
+	printf("Enter two numbers: \n");
+	
+	int number1;
+	int number2;
+	scanf("%d%d", &number1, &number2);
+		
+	//int result = MINOF2(number1, number2);
+	printf("Numbers %d and %d\n", number1, number2);
+	printf("The minimum of %d and %d is: %d\n", number1, number2, MINOF2(number1, number2));
+	
+	return 0;
+}
