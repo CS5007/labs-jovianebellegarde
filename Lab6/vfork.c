5ac482415bf11767a53c88d3980fbe354fa29a5a
Joviane Bellegarde <jbellegarde@login-students.ccs.neu.edu>
2021-02-28 15:23:00 -0500

    added more code to vfork

--- Lab6/vfork.c
+++ Lab6/vfork.c
@@ -12,16 +12,16 @@ void paint(...) {
 
 int main(int argc, char** argv) {
 
-	int numberOfArtists = 5; // How many child processes do we want?
+	int numberOfArtists = 8; // How many child processes do we want?
 
 	pid_t pid;
 	// main loop where we fork new threads
 	for(int i = 0; i < numberOfArtists; i++) {
 		// (1) Perform a fork
-		pid = 4;
+		pid = vfork();
 
 		// (2) Make only the child do some work (i.e pain) and then terminate.
-		if (pid == 5) {
+		if (pid == 0) {
 			// TODO: make child paint
 			// TODO: Then terminate/exit child
 
